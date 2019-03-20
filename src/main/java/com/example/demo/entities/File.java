330325975adf01eb7faf9a137a4b041f2d948971
A752328 <A752328@MC0AG4YC.ww930.my-it-solutions.net>
2019-03-20 16:42:27 +0000

    first commit

---
+++ src/main/java/com/example/demo/entities/File.java
@@ -0,0 +1,44 @@
+package com.example.demo.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class File {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Length(max=30)
+	private String nomFile;
+	@DateTimeFormat(pattern="dd/MM/yyy")
+	@Temporal(value=TemporalType.DATE)
+	private Date date;
+	private Long size;
+	@ManyToOne
+	private ServerCategorie serverCategorie;
+
+}
---
+++ src/main/java/com/example/demo/entities/ServerCategorie.java
@@ -0,0 +1,38 @@
+package com.example.demo.entities;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ServerCategorie {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Length()
+	private String nomCategorie;
+	@OneToMany(mappedBy="serverCategorie",fetch=FetchType.EAGER)
+	List<File> files;
+
+}
