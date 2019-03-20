62e1f313edb9f91ec27db739b034847725eb9c02
A752328 <A752328@MC0AG4YC.ww930.my-it-solutions.net>
2019-03-20 17:07:57 +0000

    upload file

--- src/main/java/com/example/demo/web/LoggController.java
+++ src/main/java/com/example/demo/web/LoggController.java
@@ -1,9 +1,34 @@
 package com.example.demo.web;
 
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.StorageService;
+import com.example.demo.metier.FileImplementation;
 
 @Controller
-public class LoggController {
+public class LoggController{
+@Autowired
+private FileImplementation fileImplementation;
+private final StorageService storageService;
 
+@Autowired
+public LoggController(StorageService storageService) {
+    this.storageService = storageService;
+}
+
+@GetMapping("/")
+public String listUploadedFiles(Model model) throws IOException {
+
+
+
+    return "uploadForm";
+}
 	
 }
